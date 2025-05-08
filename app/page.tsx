b99bac30eddbfcdbc7c2a7232e19b10466c568e4
LSWasTaken <heavenlyheadband@gmail.com>
2025-05-08 18:26:32 +0800

    a2

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { saveScore, getLeaderboard } from '../firebase'; // Confirm this path
+import { saveScore, getLeaderboard } from '../firebase';
 import Head from 'next/head';
 
 export default function Home() {
